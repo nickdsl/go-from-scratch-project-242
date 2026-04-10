34f61b342547919fd8fa2fff46acd4cc655ba61d
Nikolay Kuznetsov <n.kuznetsov@studyworld.io>
2026-04-10 12:29:46 +0700

    step_2 | теперь при указании флага -h выводится стандартная подсказка

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"log"
+	"context"
+	"github.com/urfave/cli/v3"
 )
 
 func main() {
-	var message string = "Hello from Hexlet!"
-	fmt.Println(message)
+	cmd := &cli.Command{
+		Name:  "hexlet-path-size",
+		Usage: "print size of a file or directory",
+		Action: func(context.Context, *cli.Command) error {
+			var message string = "Hello from Hexlet!"
+			fmt.Println(message)
+			return nil
+		},
+	}
 
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
+	
 }
 
