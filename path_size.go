addf06e6a964eca45d5fa4d1e65ae72009826c43
Nikolay Kuznetsov <n.kuznetsov@studyworld.io>
2026-04-10 21:51:28 +0700

    step_5 | формат отдельно

--- path_size.go
+++ path_size.go
@@ -3,14 +3,17 @@ package code
 import (
 	"fmt"
 	"os"
+	//"slices"
+	//"strings"
 )
 
 func GetPathSize(path string) (string, error) {
+	myFormat := "B"
 	mySize := int64(0)
 
 	fileInfo, err := os.Lstat(path)
 	if err != nil {
-		return fmt.Sprintf("%dB", mySize), err
+		return fmt.Sprintf("%d%s", mySize, myFormat), err
 	}
 	// если мы тут, то нам получилось без ошибок получить какую то информацию
 	// которая находится в fileInfo
@@ -20,8 +23,9 @@ func GetPathSize(path string) (string, error) {
 	// если Не каталог, то просто возвращаем размер файла
 	if !(fileInfo.IsDir()) {
 		mySize += fileInfo.Size()
-		return fmt.Sprintf("%dB", mySize), nil
+		return fmt.Sprintf("%d%s", mySize, myFormat), nil
 	}
+
 	// если мы доходим до этого места, то мы в ситуации, когда нам на вход подали не файл, а каталог
 
 	// для начала нужно прочитать каталог и получить его содержимое	
@@ -29,7 +33,7 @@ func GetPathSize(path string) (string, error) {
 
 	// возникли ошибки при чтении каталога
 	if err != nil {
-		return fmt.Sprintf("%dB", mySize), err
+		return fmt.Sprintf("%d%s", mySize, myFormat), err
 	}
 
 	// если мы тут, ошибок нет, работаем с полученными данными
@@ -40,7 +44,7 @@ func GetPathSize(path string) (string, error) {
 			fileInfo, err := file.Info()
 			// обрабатываем ошибку получения информации о файле
 			if err != nil {
-                                return fmt.Sprintf("%dB", mySize), err
+                                return fmt.Sprintf("%d%s", mySize, myFormat), err
                         }
 			// кажется здесь нужно реализовать в будущем обработку отдельно для скрытых файлов	
 			mySize += fileInfo.Size()
@@ -50,5 +54,11 @@ func GetPathSize(path string) (string, error) {
 			continue
 		}
 	}
-	return fmt.Sprintf("%dB", mySize), nil
+	return fmt.Sprintf("%d%s", mySize, myFormat), nil
+}
+
+func formatSize(size int64, format string) (int64, string) {
+	// описываем срез возможных форматов по мере возрастания
+	//formats := []string{"B", "KB", "MB", "GB", "TB", "PB", "EB"}
+	return 0, ""
 }
